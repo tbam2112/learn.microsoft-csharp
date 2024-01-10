e5c5d465dc9cf482a9b25b082295f05c413dde4d
tbam2112 <trbushnell@live.com>
2024-01-10 15:26:49 -0600

    challenge project involving data types and conversion

---
+++ challenge-projects/data-types-and-conversions/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Problem 1:");
+        string message = "";
+        float total = 0;
+
+        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+        for (int i = 0; i < values.Length; i++)
+        {
+            float number = 0;
+            if (float.TryParse(values[i], out number))
+            {
+                total += number;
+            }
+            else
+            {
+                message += values[i];
+            }
+        }
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Total: {total}");
+
+
+        ////////////////////////////////////////////////////////////////
+
+
+        Console.WriteLine("\nProblem 2:");
+
+        int value1 = 12;
+        decimal value2 = 6.2m;
+        float value3 = 4.3f;
+
+        int result1 = value1 / Convert.ToInt32(value2);
+        // Hint: You need to round the result to nearest integer (don't just truncate)
+        Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+        decimal result2 = value2 / (decimal)value3;
+        Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+        float result3 = value3 / (float)value1;
+        Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+    }
+}
---
+++ challenge-projects/data-types-and-conversions/README.md
@@ -0,0 +1,26 @@
+# Convert data types using casting and conversion techniques
+
+This challenge includes two problems.
+
+1. Combine string array data as strings and as integers.
+	* starter code: `string[] values = { "12.3", "45", "ABC", "11", "DEF" };`
+	* Rule 1: If the value is alphabetical, concatenate it to form a message.
+	* Rule 2: If the value is numeric, add it to the total.
+	
+2. Output math operations as specific number types.
+	* starter code:
+	```
+		int value1 = 12;
+		decimal value2 = 6.2m;
+		float value3 = 4.3f;
+
+		// Your code here to set result1
+		// Hint: You need to round the result to nearest integer (don't just truncate)
+		Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+		// Your code here to set result2
+		Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+		// Your code here to set result3
+		Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+	```
